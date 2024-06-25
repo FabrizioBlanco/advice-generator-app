6199fd870f4da4fcf64c7dc39805390f56d497cb
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2024-06-25 16:32:06 -0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
+async function getUrl() {
+  const advice = await fetch("https://api.adviceslip.com/advice").then(data => data)
+  const data = await advice.json()
+  return data.slip
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [advice, setAdvice] = useState("")
+  const [adviceId, setAdviceId] = useState()
+  async function getRandomAdvice() {
+    const adviceText = await getUrl()
+    setAdvice(adviceText.advice)
+    setAdviceId(adviceText.id)
+  }
+  useEffect(() => {
+    getRandomAdvice()
+  }, [])
 
   return (
-    <>
-     
-    </>
+    <main className='container'>
+      <span id='adviceId'>ADVICE #{adviceId}</span>
+      <p id='adviceText'>"{advice}"</p>
+      <img id='divider' src="public\images\pattern-divider-mobile.svg" alt="divider pattern" />
+      <div className='diceContainer' onClick={() => getRandomAdvice()}>
+        <img id="dice" src="public\images\icon-dice.svg" alt="dice" />
+      </div>
+    </main>
   )
 }
 
-export default App
+
